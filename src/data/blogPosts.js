ab84057a608a8056c2946c96d93e0720443707ec
Ayan Nayyer <ayannayyer@gmail.com>
2025-02-01 20:52:50 +0530

    added blog posts

--- src/data/blogPosts.js
+++ src/data/blogPosts.js
@@ -36,7 +36,7 @@ Stuck on the seat facing opposite of the direction of the train, Wanting to watc
         category: 'philosophical',
         excerpt: 'Start to question....',
         date: '2024-10-01',
-        content: `<h2>Why does things happen?</h2>
+        content: `
         <p>I've started questioning things.</p>
         <p>Why does anything matter? For that matter, why do the petals of cherry blossoms fall at the rate of 5 centimeters per second and not 10? Does it all end up adding to the little things in the end?</p>
         <p>Everything we do is supposed to affect something in the larger picture, like a series of dominos. But why is it that something you'vIe been longing for, for so long, isn't left for destiny to decide? Why do you have to care and shape it into what you want it to be?</p>
@@ -78,7 +78,7 @@ came the command to knot the loose threads of life’s sleeves.</p>
         category: 'philosophical',
         excerpt: '9/11.....',
         date: '2024-02-20',
-        content: `<h2>The Difference</h2>
+        content: `
         <p>The world wouldn't care much if it'd have been the fourth aircraft also hitting the US capitol, but then the difference is what you create by not allowing the wrongdoer succeed.</p>
         <p>[Insight: The United Airlines flight 93, which was hijacked along with the three more flights on 11th of September, 2001 ( 2 of which hit the towers of World trade center) was confined to reach its destination The US Capitol because of the coup of the passengers over the terrorist and crashed on an empty land]</p>`
       
@@ -89,25 +89,60 @@ came the command to knot the loose threads of life’s sleeves.</p>
         category: 'technical',
         excerpt: 'the power of investment....',
         date: '2020-05-01',
-        content: `<h2>To Invest or to Not??</h2>
-        <p>the power of investment....</p>`
+        content: `
+        <p>Okay let me start with a simple question- If you Save a rupee on the very first day of the month and go on doubling it for the rest of the month, what would the total saving end up to?<br />
+
+-1 lac?</p>
+
+-10 lac?<br />
+
+-100 lac?<br /><br />
+
+<p>No dear, Not even close! It would leave you with a hefty amount of more than  '5368 lac'.
+
+A magic? No, just the power of investing. 
+
+Look, it is a slow process. It isn't like investing a lac and getting a crore on the next day but patience and constant investment is what it takes to take you to heights, Like in the earlier example we took, we looked at a period of 30 days. But on the 18th day your one rupee get only doubled to 1.3 lac. But here's the catch, In a span of just 12 days this 1.3 lac gives you an additional 5367 lac.
+
+Look, I understand you won't receive schemes where you can double your money every day, but still it's quite a lot better than making a stack of money in your home.
+
+Moreover, Investing adds up to a country's development and safekeeps your money with some interests.</p>
+
+Now, Coming upon where to invest: So, there are many such investment promotion and facilitation agency which you can find near you. For online investments you have apps like:
+
+<ul>* Investr</ul>
+
+<ul>* Paytm Money</ul>
+
+<ul>* Groww</ul>
+
+and many more.</p>
+<p>Talking about my personal investments, I like to invest in Crypto markets, because of the freedom and volatility it provides.
+Some common apps through which you can invest in cryptocurrencies are:
+
+
+<ul>* Binance</ul>
+
+<ul>* Coindcx</ul>
+
+<ul>* Wazirx</ul>
+
+
+Unlike stock markets, Cryptocurrencies don't restrict you to a certain time zone in which you can trade and also cryptos are way more rewarding than stocks or any other given investment plans, provided you study the charts and trade safely.</p>
+<p><b>Long story Short: Investment isn't a short term temporary joy but is very much similar to a long term incessant happiness.</p>
+<div class="blog-image-container">
+    <img src="/investing.jpeg" alt="Investing Breakdown" class="blog-image" />
+    <b><u><p class="image-caption">For those who were wondering :p</p></u></b>
+</div>
+`
       },
       {
         id: 8,
-        title: 'Top 5 Hollywood Movies',
-        category: 'entertainment',
-        excerpt: 'To change your perspective....?',
-        date: '2020-07-10',
-        content: `<h2>Top 5 Hollywood Movies</h2>
-        <p>To change your perspective....?</p>`
-      },
-      {
-        id: 9,
         title: 'Love or Like?',
         category: 'philosophical',
         excerpt: 'Some Weird Shit....',
         date: '2020-05-02',
-        content: `<h2>Love or Like?</h2>
+        content: `
         <p>I don't love anyone, I don't like anyone but,
 
 I do 'live' some people.
@@ -124,29 +159,74 @@ You live somebody,You are now experiencing what he/she does. Be it sorrow, Be it
 <p>"live' isn't something as shallow as like it's neither as deep as 'love'. It's something which binds two life in one.</p>`
       },
       {
-        id: 10,
+        id: 9,
         title: 'Taxonomy FY 23-24/ INDIA',
         category: 'technical',
         excerpt: 'lets learn about old and new tax regime...',
         date: '2024-02-20',
-        content: `<h2>Taxonomy FY 23-24/ INDIA</h2>
-        <p>lets learn about old and new tax regime...</p>`
+        content: `
+        <p>Not a Commerce Student but here is what I understood about the change in the new Tax Regime: </p>
+
+<p>In the new Tax Regime the no tax slab has been increased to an annual income of upto Rs.7 lacs but this includes no deduction under section 80c(which included deduction under investments in elss/ppf).So, one has to pay wholly on the amount earned annually So, now what happens is an individual earning Rs.8.5 lacs annually has to pay no tax for the first Rs.3 lacs, then 5% for the next Rs.3 lacs and 10% for the remaining Rs.1.5 lacs(15000+15000=30000).</p>
+
+<div class="blog-image-container">
+    <img src="/tax_regime.jpg" alt="Tax Regime Breakdown" class="blog-image" />
+    <b><u><p class="image-caption">Tax Regime Breakdown for FY 23-24 CURRENT v/s PREVIOUS</p></u></b>
+</div>
+<p>Whereas there is no change in the Old tax regime, which had a no tax slab upto 5 lacs/annum. But the part worth noticing is the deductions under section 80c, which could be availed under this old regime which includes investments under several funds that won't fall under taxations.
+
+
+<p>So overall, is the change any beneficial? As, in the old tax regime one could've easily availed no tax using all the deductions even if they had an income of 8-9 lacs/annum. </p>
+
+
+<p>Even though, one has the choice of choosing any of the available two tax regime as of now, No one knows which FY would be the last for it. </p>`
       },
       {
-        id: 11,
+        id: 10,
         title: 'HIMYM - The Ending.',
         category: 'entertainment',
         excerpt: 'the ending?was it any good...',
-        date: '2024-07-04'
+        date: '2024-07-04',
+        content: `
+        <p>Talking about the legen.......dary show 'How I met your mother' A situational comedy produced by CBS, Where the narrator tells about his story to his children of meeting their children.
+
+If you search for the top five sitcoms ever made one of those definitely would be 'HIMYM' but at the same time when you search for the five series with the worst ending, it'd definitely pop up. Why the hate?</p>
+<p>The series which ran for 9 years (2006-2014) spent the time to build up the cast's character, but the ending we got was contrary to the same.</p>
+<p>The narrator who dated Robin several times throughout the series and in the course of the story discovered that they both didn't work out well, was forced to get with each other in the end.
+
+Barney, The superawesome character who finally settled with robin after experiencing that his single life was nothing but a mess was forced to get single again(divorcing robin). The character for which audience waited for several years was introduced to us very closely but was killed by the show's writers.</p>
+<p>But then, the writers had been giving hints about the same from the beginning as when Ted first met robin he said "I am gonna marry her someday", in S1 itself, and when Ted runs to the mother's house to get the extra 45 days with her and many more such examples, justifying the writer's ending.</p>
+
+
+<p>But the show is one of a kind, humor with a good story, sometimes the plot would leave you crying, sometimes you won't be able to control your laughter. You will feel that the group was never of 5 but of '6'.</p>`
+
+
       },
       {
-        id: 12,
+        id: 11,
         title: 'Mr. Beast - The Next Big Thing?',
         category: 'entertainment',
         excerpt: 'Is he or is he not going to make it?...',
         date: '2020-03-14',
-        content: `<h2>Mr. Beast - The Next Big Thing?</h2>
-        <p>Is he or is he not going to make it?...</p>`
+        content: `
+        <p>A major part of my audience must have already heard of the content creator Jimmy Donaldson AKA Mr. Beast.</p>
+
+
+<p>For others, He is a Youtuber with around 32 million subscribers(as of 14th March, 2020) and around 62 million Subs combining all his other youtube channels. He started his main channel in 2012 and is famous for his unique and heart-warming videos along with the social work he does i.e. Running a campaign to plant 20 Million trees, Donating money to poors (which is ofc the content of his videos.)</p>
+
+
+<p>Philanthropist might be the just word to describe him. As in his own words, he throws all the revenue he generates from his past videos into the next one he's working upon.</p>
+
+
+<p>Besides Youtube, he is often seen active on Twitter and loves to interact with people in a fun way, see for yourself:</p>
+<p>Okay So why is he the next big thing?
+
+Apparently I like to believe in this quote which goes as:
+
+
+<h3>"If You do good, Good will come back to you"</h3>
+
+And also who doesn't love generous people</p>`
       }
     // Add more posts following the same structure
   ];
